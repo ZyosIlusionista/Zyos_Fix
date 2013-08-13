7a584ae357e404fd080c9a528608fe650398369c
alejotemplario <alejo.temp@gmail.com>
2013-08-12 19:33:35 -0500

    cambio en index

--- Aplicacion/Modulos/Gestion/Controladores/Changepass.php
+++ Aplicacion/Modulos/Gestion/Controladores/Changepass.php
@@ -11,7 +11,7 @@
 			
 			$Parametros = AyudasSessiones::InformacionSessionControlador(true);
 			$Validacion = new NeuralJQueryValidacionFormulario;
-            $Validacion->IgualACampo('Passuno', 'Passdos', 'Las Contraseñas No Coinciden');
+            $Validacion->IgualACampo('Passuno', 'Passdos', 'Las ContraseÃ±as No Coinciden');
             $Validacion->RangoLongitud('Passuno', '8', '20', 'El campo es de 8 a 20 caracteres');
 			$Validacion->SubmitHandler(NeuralJQueryAjax::EnviarFormularioPOST('Formulario', 'respuesta', NeuralRutasApp::RutaURL('Ajax/CambioPassword'), true, 'GESTION'));
             $Script[] = $Validacion->MostrarValidacion('Formulario');
